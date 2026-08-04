7157471c265303a99811c73a2bc983c176e3166e
Kushal Pandita <kushalpandita007@gmail.com>
2026-08-04 09:09:33 +0530

    Time: 4 ms (92.59%) | Memory: 43.8 MB (97.19%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,26 +1,64 @@
 class Solution {
     public String reverseWords(String s) {
-        List<String> words = new ArrayList<>();
+        
+        char arr[] = s.toCharArray();
 
+        reverse(arr,0,s.length()-1);
+        reverseWords(arr);
+        return cleanSpaces(arr);
+    }
+
+    public void reverse(char arr[], int left, int right){
+        while(left<right){
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+   
+    public void reverseWords(char arr[]){
+
+        int n = arr.length;
+        int start = 0;
+        int end = 0;
+        while(start<n){
+            while(start<n && arr[start] == ' '){
+                start++;
+            }
+            end = start;
+            while(end < n && arr[end] != ' '){
+                end++;
+            }
+            reverse(arr,start,end-1);
+            start = end;
+        }
+    }
+                    
+    public String cleanSpaces(char[] arr){
+        int n = arr.length;
         int i = 0;
-        int n = s.length();
+        int j = 0;
 
         while(i<n){
-            while(i<n && s.charAt(i)== ' '){
-                i++;
-            }
-            int start = i;
-
-            while(i<n && s.charAt(i) != ' '){
+            while(i<n && arr[i] == ' '){
                 i++;
             }
 
-            if(start < i){
-                words.add(s.substring(start,i));
+            while(i<n && arr[i] != ' '){
+                arr[j++] = arr[i++];
+            }
+
+            while(i<n && arr[i] == ' '){
+                i++;
+            }
+
+            if(i<n){
+                arr[j++] = ' ';
             }
         }
-
-        Collections.reverse(words);
-        return String.join(" ",words);
+        
+        return new String(arr,0,j);
     }
 }
